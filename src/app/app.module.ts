307ee3416d5793e9638db736af7719fbef59d3a3
Brent Miller <brentwmiller93@gmail.com>
2017-11-29 21:24:53 -0500

    Adds nav & feature components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
+import { FeatureComponent } from './component/feature/feature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeatureComponent
   ],
   imports: [
     BrowserModule
