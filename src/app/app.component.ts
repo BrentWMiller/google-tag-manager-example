3006db491b560a03b431e3f15d3f0fbae28d48ea
Brent Miller <brentwmiller93@gmail.com>
2017-11-30 20:42:13 -0500

    Updates app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,42 +17,42 @@ export class AppComponent {
         'description': 'Hendrerit vitae massa',
         'link': 'Link 1',
         'linkAlt': 'Link 1 Alt',
-        'color': '#F44336'
+        'color': '#0D47A1'
       },
       {
         'title': 'Sed a tristique',
         'description': 'Integer ac sapien vehicula',
         'link': 'Link 2',
         'linkAlt': 'Link 2 Alt',
-        'color': '#E91E63'
+        'color': '#4788f4'
       },
       {
         'title': 'Sagittis hendrerit eros nibh ut magna',
         'description': 'Donec viverra sem antes.',
         'link': 'Link 3',
         'linkAlt': 'Link 3 Alt',
-        'color': '#9C27B0'
+        'color': '#304FFE'
       },
       {
         'title': 'Donec viverra sem ante, sit amet congue quis.',
         'description': 'Pellentesque tempor enim',
         'link': 'Link 4',
         'linkAlt': 'Link 4 Alt',
-        'color': '#FF1744'
+        'color': '#406ed0'
       },
       {
         'title': 'Integer ac sapien vehicula',
         'description': 'Phasellus eu mi nibh tempor enim.',
         'link': 'Link 5',
         'linkAlt': 'Link 5 Alt',
-        'color': '#F50057'
+        'color': '#427be5'
       },
       {
         'title': 'Sit amet fermentum libero',
         'description': 'Donec viverra sem ante, sit amet.',
         'link': 'Link 6',
         'linkAlt': 'Link 6 Alt',
-        'color': '#D500F9'
+        'color': '#5ec9f8'
       }
     ];
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './component/navigation/navigation.component';
 import { FeatureComponent } from './component/feature/feature.component';
+import { HeroComponent } from './component/hero/hero.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FeatureComponent
+    FeatureComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
