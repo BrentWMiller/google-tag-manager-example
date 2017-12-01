5aa85028e62c2624e49719ba16d8da0b3036eadb
Brent Miller <brentwmiller93@gmail.com>
2017-11-30 20:42:05 -0500

    Updates feature component to pass clicks to GTM

--- src/app/component/feature/feature.component.ts
+++ src/app/component/feature/feature.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'gtm-feature',
-  templateUrl: './feature.component.html',
-  styleUrls: ['./feature.component.scss']
+	selector: 'gtm-feature',
+	templateUrl: './feature.component.html',
+	styleUrls: ['./feature.component.scss']
 })
 export class FeatureComponent implements OnInit {
 
-  @Input() title: string;
-  @Input() description: string;
-  @Input() link: string;
-  @Input() linkAlt: string;
-  @Input() color: string;
+	@Input() title: string;
+	@Input() description: string;
+	@Input() link: string;
+	@Input() linkAlt: string;
+	@Input() color: string;
 
-  constructor() { }
+	dataObject: any = [];
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	public linkClicked(type: string): void {
+
+		this.dataObject = {
+			'event': 'link_clicked',
+			'link_type': type
+		};
+
+		console.log(this.dataObject);
+
+		// window['dataLayer'].push(this.dataObject);
+	}
 
 }
